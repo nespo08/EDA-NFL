6af399b2f8e0c38f2b42b3af56e5324a525ae4d9
Nicholas Esposito <nesposito@ufl.edu>
2022-06-22 21:42:46 -0400

    Created csv file and read in for data frame

--- Comp-Percentage-And-Clustering.R
+++ Comp-Percentage-And-Clustering.R
@@ -4,27 +4,29 @@ library(nflreadr)
 library(ggplot2)
 library(tidyverse)
 
-nfl_2021_data <- nflreadr::load_pbp(2021, file_type = "rds")
+# nfl_2021_data <- nflreadr::load_pbp(2021, file_type = "rds")
+# 
+# nfl_passing_plays <- nfl_2021_data %>%
+#   filter(play_type == "pass", season_type == "REG", 
+#          !is.na(epa), !is.na(posteam), posteam != "") %>%
+#   select(# Player info attempting the pass:
+#     passer_player_name, passer_player_id, posteam, 
+#     # Info about the pass:
+#     complete_pass, interception, yards_gained, touchdown, 
+#     pass_location, pass_length, air_yards, yards_after_catch, epa, wpa,
+#     shotgun, no_huddle, qb_dropback, qb_hit, sack,
+#     # Context about the receiver:
+#     receiver_player_name, receiver_player_id   ,
+#     # Team context:
+#     posteam, defteam, posteam_type, 
+#     # Play and game context:
+#     play_id, yardline_100, side_of_field, down, qtr, play_clock,
+#     half_seconds_remaining, game_half, game_id,
+#     home_team, away_team, home_score, away_score,
+#     # Description of play
+#     desc)
 
-nfl_passing_plays <- nfl_2021_data %>%
-  filter(play_type == "pass", season_type == "REG", 
-         !is.na(epa), !is.na(posteam), posteam != "") %>%
-  select(# Player info attempting the pass:
-    passer_player_name, passer_player_id, posteam, 
-    # Info about the pass:
-    complete_pass, interception, yards_gained, touchdown, 
-    pass_location, pass_length, air_yards, yards_after_catch, epa, wpa,
-    shotgun, no_huddle, qb_dropback, qb_hit, sack,
-    # Context about the receiver:
-    receiver_player_name, receiver_player_id   ,
-    # Team context:
-    posteam, defteam, posteam_type, 
-    # Play and game context:
-    play_id, yardline_100, side_of_field, down, qtr, play_clock,
-    half_seconds_remaining, game_half, game_id,
-    home_team, away_team, home_score, away_score,
-    # Description of play
-    desc)
+nfl_passing_plays <- read.csv("nfl-passing-plays-2021.csv")
 
 # Preview data
 head(nfl_passing_plays, n = 50)
